2403d7704ccbc3c526be299cd4e0c16826d3fefb
uma-kamuni <kamuniumaramesh@gmail.com>
2021-12-18 15:41:08 -0800

    Welcome To Line Comparison Program

---
+++ src/com/bridgelabz/LineComparisonProgram.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class LineComparisonProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line-Comparison Program using OOPS ");
+	}
+
+}
